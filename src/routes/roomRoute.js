353f6e2229628d88aff5acbf64edf90f24498c6f
Faisal Hilmi <faisalhilmi082@gmail.com>
2024-05-30 00:47:34 +0700

    modify: roomRoute.js

--- src/routes/roomRoute.js
+++ src/routes/roomRoute.js
@@ -6,11 +6,12 @@ const {
 } = require("../controller/roomController");
 const bucketUpload = require("../utils/uploadToBucket");
 const multer = require("../middleware/uploadImage");
+const { verifyToken } = require("../middleware/authToken");
 
 const router = express.Router();
 
-router.get("/room", getAllRoomController);
-router.get("/room/:room_id", getAllRoomByIdController);
+router.get("/room", verifyToken, getAllRoomController);
+router.get("/room/:room_id", verifyToken, getAllRoomByIdController);
 router.post("/room", multer.none(), createRoomController);
 
 module.exports = router;
