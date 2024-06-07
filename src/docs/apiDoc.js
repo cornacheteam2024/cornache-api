5409ded3d765a2801e3b63aa651b3209b907f809
rizalalif <rizalalif03@gmail.com>
2024-06-07 15:43:53 +0700

    all of documentation

--- src/docs/apiDoc.js
+++ src/docs/apiDoc.js
@@ -29,7 +29,7 @@ const apiDocumentation = {
     openapi: '3.0.1',
     info: {
       version: '1.3.0',
-      title: 'My REST API - Documentation',
+      title: 'Cornache API - Documentation',
       description: 'Description of my API here',
       termsOfService: 'https://mysite.com/terms',
       contact: {
@@ -186,9 +186,6 @@ const apiDocumentation = {
                 },
             }
         },
-        // license: {
-        //     name: 'Apache 2.0',
-        //     url: 'https://www.apache.org/licenses/LICENSE-2.0.html',
 
         tags: [
             {
